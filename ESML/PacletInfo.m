2b0bbe324cd56ce11ec6b527c0767f4399773a51
fpedro <fp3dr0@gmail.com>
2015-09-16 18:46:06 +0100

    unzip

---
+++ ESML/PacletInfo.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+Paclet[
+Name -> "ESML",
+Version -> "0.1.0",
+MathematicaVersion -> "8+",
+Extensions -> {{
+    "Documentation", 
+    Resources -> {
+        "ReferencePages/ESML"
+    }, 
+    Language -> "English"
+}}
+]
